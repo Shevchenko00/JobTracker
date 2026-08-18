368693857884c7f47ed6c3e1848f5a8f516bea6b
Shevchenko00 <shevchenko.sasha2005@gmail.com>
2026-08-18 20:52:36 +0300

    feat: add psycopg requirements

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,24 +79,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 
-if env.bool('DB', default=False):
-    DATABASES = {
-        'default': {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("POSTGRES_DB", "jobs"),
-            "USER": os.getenv("POSTGRES_USER", "jobs"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "jobs_password"),
-            "HOST": os.getenv("POSTGRES_HOST", "jobs_postgres"),
-            "PORT": os.getenv("POSTGRES_PORT", "5432"),
-        }
+
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "jobs"),
+        "USER": os.getenv("POSTGRES_USER", "jobs"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "jobs_password"),
+        "HOST": os.getenv("POSTGRES_HOST", "jobs_postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        },
-    }
+}
+
 
 
 # Password validation
